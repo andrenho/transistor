033a42dcedbcb731644776452fb352e5698a5d3d
Andre Wagner <WagnerAndre@JohnDeere.com>
2024-12-31 09:39:43 -0600

    .

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,7 @@
 #include "ui/ui.hh"
 
 #include <chrono>
+using namespace std::chrono_literals;
 
 #include "engine/sandbox/sandbox.hh"
 
@@ -14,6 +15,8 @@ int main()
     auto last_frame = hr::now();
     while (ui.running()) {
         auto new_frame = hr::now();
+        while (hr::now() < new_frame + 6ms)   // simulate for 6 ms
+            sandbox.simulate();
         ui.update(new_frame - last_frame);
         ui.render();
         last_frame = new_frame;
