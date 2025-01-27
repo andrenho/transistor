878810cfa2df73ff134c8734cebe62682a84d6c5
Andre Wagner <WagnerAndre@JohnDeere.com>
2025-01-27 10:54:28 -0600

    .

--- src/ui/devices/board/boardeditor.hh
+++ src/ui/devices/board/boardeditor.hh
@@ -43,7 +43,13 @@ private:
         tile,
         board_top_left, board_top, board_top_right, board_left, board_right, board_bottom_left, board_bottom, board_bottom_right,
         wire_top_on_north_1, wire_top_on_east_1, wire_top_on_west_1, wire_top_on_south_1,
-        wire_top_off_north_1, wire_top_off_east_1, wire_top_off_west_1, wire_top_off_south_1;
+        wire_top_off_north_1, wire_top_off_east_1, wire_top_off_west_1, wire_top_off_south_1,
+        ic_dot_n, ic_dot_w, ic_dot_e, ic_dot_s,
+        ic_pin_n, ic_pin_w, ic_pin_e, ic_pin_s,
+        ic_logic_nw, ic_logic_n, ic_logic_ne, ic_logic_e, ic_logic_w, ic_logic_sw, ic_logic_s, ic_logic_se, ic_logic_center,
+        ic_digital_nw, ic_digital_n, ic_digital_ne, ic_digital_e, ic_digital_w, ic_digital_sw, ic_digital_s, ic_digital_se, ic_digital_center,
+        ic_memory_nw, ic_memory_n, ic_memory_ne, ic_memory_e, ic_memory_w, ic_memory_sw, ic_memory_s, ic_memory_se, ic_memory_center,
+        ic_cpu_nw, ic_cpu_n, ic_cpu_ne, ic_cpu_e, ic_cpu_w, ic_cpu_sw, ic_cpu_s, ic_cpu_se, ic_cpu_center;
 };
 
 #endif //BOARDEDITOR_HH
