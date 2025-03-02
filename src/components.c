fe3f8059c3698769d9371f29b7d8ac81ef81ff75
Andre Wagner <WagnerAndre@JohnDeere.com>
2025-03-02 11:11:23 -0600

    .

--- src/components.c
+++ src/components.c
@@ -2,9 +2,42 @@
 
 #include <stdlib.h>
 #include <SDL3/SDL.h>
+#include <lauxlib.h>
 
 #include "button.lua.h"
 #include "graphics.h"
+
+//
+// GRAPHICS OBJECT
+//
+
+static int G_luaref = -1;
+
+static int graphics_load_image_base64(lua_State* L)
+{
+    return 0;
+}
+
+static int graphics_render_image(lua_State* L)
+{
+    return 0;
+}
+
+static void create_graphics_object(lua_State* L)
+{
+    static const struct luaL_Reg graph_obj[] = {
+        { "load_image_base64", graphics_load_image_base64 },
+        { "render_image",      graphics_render_image },
+        { NULL, NULL }
+    };
+    lua_newtable(L);
+    luaL_setfuncs(L, graph_obj, 0);
+    G_luaref = luaL_ref(L, -1);
+}
+
+//
+// COMPONENT OPERATIONS
+//
 
 static void load_component(ts_Transistor* t, const char* lua_code)
 {
@@ -18,7 +51,12 @@ static void load_component(ts_Transistor* t, const char* lua_code)
     // TODO - run init
 }
 
+//
+// INIT
+//
+
 void components_init(ts_Transistor* t)
 {
+    create_graphics_object(t->sandbox.L);
     load_component(t, resources_components_button_lua);
 }
