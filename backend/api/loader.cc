d96657690f2c919933ed1f388a8975890f65c120
Andre Wagner <WagnerAndre@JohnDeere.com>
2025-03-18 16:24:18 -0500

    .

--- backend/api/loader.cc
+++ backend/api/loader.cc
@@ -4,11 +4,55 @@
 #include <string>
 using namespace std::string_literals;
 
+//
+// dev mode
+//
+
+#ifndef NDEBUG
+
 void load_transistor(lua_State* L)
 {
-#ifndef NDEBUG   // dev mode
-    if (luaL_dofile(L, "engine/contrib/tl.lua") != LUA_OK)
-        throw std::runtime_error("Error loading tl.lua: "s + lua_tostring(L, -1));
-#else            // release mode
-#endif
+    if (luaL_dostring(L, R"(
+        package.path = package.path .. ';./backend/?.lua'
+        local tl = require 'engine.contrib.tl'
+        tl.loader()
+        require 'engine.sandbox'
+    )") != LUA_OK)
+        throw std::runtime_error("Error loading `transistor`: "s + lua_tostring(L, -1));
+
+    // TODO - watch files
 }
+
+//
+// release mode
+//
+
+#else
+
+#include <vector>
+
+struct Bytecode {
+    const char* name;
+    uint8_t const* data;
+    size_t sz;
+};
+
+#include "engine/sandbox.lua.h"
+
+static std::vector<Bytecode> engine_lua = {
+#define LOAD(name) { #name, engine_##name##_lua, engine_##name##_lua_sz }
+    LOAD(sandbox),
+#undef LOAD
+};
+
+void load_transistor(lua_State* L)
+{
+    for (auto const& b : engine_lua) {
+        if (luaL_loadbuffer(L, (const char *) b.data, b.sz, b.name) != LUA_OK)
+            throw std::runtime_error("Error loading '"s + b.name + "' script: " + lua_tostring(L, -1));
+        if (lua_pcall(L, 0, 0, 0) != LUA_OK)
+            throw std::runtime_error("Error running '"s + b.name + "' script: " + lua_tostring(L, -1));
+    }
+}
+
+#endif
