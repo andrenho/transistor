1a379eff455804161553a2b7c5a9bc335e851d0a
Andre Wagner <WagnerAndre@JohnDeere.com>
2025-03-19 18:14:16 -0500

    .

--- backend/sim/simulator.c
+++ backend/sim/simulator.c
@@ -1,12 +1,24 @@
+#include <string.h>
+
 #include <lua.h>
 #include <lauxlib.h>
 
-static const luaL_Reg simulator_reg[] = {
-    { NULL, NULL },
-};
+typedef struct Simulator {
+} Simulator;
+
+static int simulator_new(lua_State* L)
+{
+    Simulator* simulator = lua_newuserdata(L, sizeof(Simulator));
+    memset(simulator, 0, sizeof(Simulator));
+    return 1;
+}
 
 int luaopen_simulator(lua_State* L)
 {
+    static const luaL_Reg simulator_reg[] = {
+        { "new", simulator_new },
+        { NULL, NULL },
+    };
     luaL_newlib(L, simulator_reg);
     return 1;
 }
